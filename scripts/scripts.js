040798dfb5b61f3c6af97b8b48436fd0c64134c7
Caelan Douglas <caelan.douglas@outlook.com>
2024-03-30 18:23:54 -0400

    COOL card transition

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,3 +2,14 @@ function coverUp() {
     document.getElementById("screen-gal").style.top = "10%";
     document.getElementById("screen-cover").style.bottom = "100%";
 }
+
+function gotoPage(from, moveTo) {
+    console.log(from);
+    var galleryChildren = document.getElementById("screen-gal").children;
+    for (let i = 0; i < galleryChildren.length; i++) {
+        if (galleryChildren[i] != from) {
+            galleryChildren[i].style.opacity = "0%";
+        }
+        from.style.left = "" + moveTo + "%";
+    }
+}
